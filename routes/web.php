20eca9309322a6fbe76f6ac69e75179983e157c4
Kolesen Tatsiana <aniskova007@mail.ru>
2022-09-23 20:45:18 +0300

    HW_20 format

--- routes/web.php
+++ routes/web.php
@@ -25,58 +25,63 @@ Route::get('/', [MainController::class, 'index'])
 Route::get('/about', [MainController::class, 'aboutUs'])
     ->name('home.about');
 
-Route::group(['prefix' => '/movies', 'middleware' => 'auth'], function (){
-    Route::get('', [MovieController::class, 'list'])
-        ->name('movies');
+Route::group(
+    ['prefix' => '/movies', 'middleware' => 'auth'],
+    function () {
+        Route::get('', [MovieController::class, 'list'])
+            ->name('movies');
 
-    Route::get('/create', [MovieController::class, 'createForm'])
-        ->name('movies.createForm');
+        Route::get('/create', [MovieController::class, 'createForm'])
+            ->name('movies.createForm');
 
-    Route::post('/create', [MovieController::class, 'create'])
-        ->name('movies.create');
+        Route::post('/create', [MovieController::class, 'create'])
+            ->name('movies.create');
 
-    Route::get('/{movie}', [MovieController::class, 'show'])
-        ->name('movies.show');
+        Route::get('/{movie}', [MovieController::class, 'show'])
+            ->name('movies.show');
 
-    Route::get('/{movie}/edit', [MovieController::class, 'editForm'])
-        ->name('movies.edit.form');
+        Route::get('/{movie}/edit', [MovieController::class, 'editForm'])
+            ->name('movies.edit.form');
 
-    Route::post('/{movie}/edit', [MovieController::class, 'edit'])
-        ->name('movies.edit');
+        Route::post('/{movie}/edit', [MovieController::class, 'edit'])
+            ->name('movies.edit');
 
-    Route::post('/{movie}/delete', [MovieController::class, 'delete'])
-        ->name('movies.delete');
-});
+        Route::post('/{movie}/delete', [MovieController::class, 'delete'])
+            ->name('movies.delete');
+    }
+);
 
-Route::group(['prefix' => '/contact'], function (){
-    Route::get('', [ContactController::class, 'show'])
-        ->name('contact');
+Route::group(
+    ['prefix' => '/contact'],
+    function () {
+        Route::get('', [ContactController::class, 'show'])
+            ->name('contact');
 
-    Route::post('/submit', [ContactController::class, 'store'])
-        ->name('contact.store');
-});
+        Route::post('/submit', [ContactController::class, 'store'])
+            ->name('contact.store');
+    }
+);
 
-Route::group(['prefix' => '/sing-up'], function (){
-    Route::get('', [UserController::class, 'singUpForm'])
-    ->name('sing-up.Form');
+Route::group(
+    ['prefix' => '/sing-up'],
+    function () {
+        Route::get('', [UserController::class, 'singUpForm'])
+            ->name('sing-up.Form');
 
-    Route::post('', [UserController::class, 'singUp'])
-        ->name('sing-up');
-});
+        Route::post('', [UserController::class, 'singUp'])
+            ->name('sing-up');
+    }
+);
 
 Route::get('/verify-email/{id}/{hash}', [UserController::class, 'verifyEmail'])
     ->name('verify.email');
 
-    Route::get('/login', [LoginController::class, 'loginForm'])
-        ->name('login');
+Route::get('/login', [LoginController::class, 'loginForm'])
+    ->name('login');
 
-    Route::post('/login', [LoginController::class, 'loginIn'])
-        ->name('login-in');
+Route::post('/login', [LoginController::class, 'loginIn'])
+    ->name('login-in');
 
 
 Route::post('/logout', [LoginController::class, 'logout'])
     ->name('logout');
-
-//Auth::routes();
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
