8c118ba5d62a392bff29198d114eb5244f1011d0
kolesenT <aniskova007@mail.ru>
2022-11-11 22:48:03 +0300

    HWm25 edit

--- frontend-app/src/Thermometer/TemperatureControl.jsx
+++ frontend-app/src/Thermometer/TemperatureControl.jsx
@@ -2,33 +2,29 @@ import { useState } from "react";
 // import './TemperatureControl.css';
 import './TemperatureControl.css'
 
-function TemperatureControl(){
+function TemperatureControl() {
+  const [temp, setTemp] = useState(10);
 
-    const [temp, setTemp] = useState(10);
+  const rise = () => {
+    temp < 30 && setTemp(temp + 1);
+  }
+  const drop = () => {
+    temp > 0 && setTemp(temp - 1);
+  }
 
-    const rise = () =>{
-        temp < 30 && setTemp(temp + 1);
-    }
-
-    const drop =() =>{
-        temp > 0 && setTemp(temp - 1);
-    }
-
-    const myClass = temp >= 15 ? " hot":" cold";
-
-    return (
-        <div className="app-container">
-          <div className="temperature-display-container">
-            <div className={"temperature-display" + myClass}  >
-                {temp}°C
-            </div>
-          </div>
-          <div className="button-container">
-            <button onClick={rise}>+</button>
-            <button onClick={drop}>-</button>
-          </div>
+  return (
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${temp >= 15 ? " hot" : " cold"}`}  >
+          {temp}°C
         </div>
-    )
+      </div>
+      <div className="button-container">
+        <button onClick={rise}>+</button>
+        <button onClick={drop}>-</button>
+      </div>
+    </div>
+  )
 }
 
 export default TemperatureControl;
